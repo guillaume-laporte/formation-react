437efa197a5083820b5fdab1df9e543197ec2ff9
guillaume-laporte <guillaume.laporte@live.fr>
2023-06-12 14:23:14 +0200

    import css dynamic

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Button.css';
+import style from './Button.module.css'
 
 // function Button() {}
 
 // approche proche de TS (on peut facilement rajouter le typage, Button:XXX)
 const Button = (props) => {
-    return <button className='Button'>Benjamin</button>
+    return <button className={style.Button + ' btn'}>Benjamin</button>
     // return <><button>Benjamin</button><button>Benjamin</button></>
 }
 
