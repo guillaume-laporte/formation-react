d58d6cd6bc5b7c0db2f4024a5ae014bd77d4cb8e
guillaume-laporte <guillaume.laporte@live.fr>
2023-06-12 14:49:57 +0200

    passage par children

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,9 +5,13 @@ import Button from './components/ui/Button/Button'
 function App() {
   return (
     <div className="App">
-      <Button></Button> <Button></Button>
+      <Button><img alt="" src="./img/ok_sub.png" /> ok</Button>
+      <Button text="cancel"></Button>
     </div>
   );
+  // <Button children={<div><img alt="" src="./img/ok_sub.png" /> <span>ok</span></div>}></Button> => {props.children}
+  // <Button><img alt="" src="./img/ok_sub.png" /> ok</Button> => => {props.children} aussi
+  // <Button text="cancel"></Button> => {props.text}
 }
 
 export default App;
--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -5,7 +5,12 @@ import style from './Button.module.css'
 
 // approche proche de TS (on peut facilement rajouter le typage, Button:XXX)
 const Button = (props) => {
-    return <button className={style.Button + ' btn'}>Benjamin</button>
+    console.log(props);
+    return (
+        <button className={style.Button + ' btn'}>
+            {undefined !== props.children ? props.children : props.text}
+        </button>
+    );
     // return <><button>Benjamin</button><button>Benjamin</button></>
 }
 
