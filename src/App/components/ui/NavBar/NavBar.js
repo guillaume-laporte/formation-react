edd8404db6b329af234efb76443011b3d4247891
guillaume-laporte <guillaume.laporte@live.fr>
2023-06-13 14:37:09 +0200

    formulaire

--- src/App/components/ui/NavBar/NavBar.js
+++ src/App/components/ui/NavBar/NavBar.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import style from './NavBar.module.css';
+import '../../../../../node_modules/bootstrap/dist/css/bootstrap.css';
+// facultatif si non usage des applications js de bootstrap
+// attention pour le js pensez à yarn add jquery et import './.../dist/jquery.js'
+// import '../../../../../node_modules/bootstrap/dist/js/bootstrap.js';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const NavBar = (props) => {
   return (
     <div className={style.NavBar} data-testid="NavBar">
-      NavBar
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#thumbnail">Thumbnail</Nav.Link>
+            <Nav.Link href="#newmeme">New meme</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
